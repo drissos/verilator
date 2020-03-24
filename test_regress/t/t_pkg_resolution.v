d3faf92c1649a57cb3e3706fe66821f29025f31c
Driss Hafdi <driss@hudson-trading.com>
2020-03-24 15:04:02 -0400

    Package resolution issue within enums
    Summary:
    Package resolution seems to error out whenever it is embedded within a typedef. We currently use the work around of importing pkg1::uint8_t directly, but that causes namespace pollution.

---
+++ test_regress/t/t_pkg_resolution.v
@@ -0,0 +1,26 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2020 by Driss Hafdi
+
+package pkg1;
+    typedef logic [7:0] uint8_t;
+endpackage
+
+package pkg2;
+    typedef enum pkg1::uint8_t {
+        a = 8'd1,
+        b = 8'd2
+    } opts;
+endpackage
+
+module t (/*AUTOARG*/
+  );
+
+  initial begin
+      $write("*-* All Finished *-*\n");
+      $finish;
+  end
+
+endmodule
+
